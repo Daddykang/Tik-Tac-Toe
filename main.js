ef71f8ccc52dd3ca8a9564d1f23942247c72868c
Daddykang <142983865+Daddykang@users.noreply.github.com>
2024-02-18 13:40:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#resetbBtn");
+let msgcnt = document.querySelector("msg-container");
+let msg = document.querySelector("#msg");f
+
+
+let turnO=true; //for playerX,playerO
+
+ const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+ ];
+
+ boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        console.log("Button was clicked");
+        if(turnO){
+            box.innerText = "O";
+            turnO = false;
+        }
+        else{
+            box.innerText = "X";
+            turnO = true ;
+        }
+        box.disabled = true;
+        checkWinner();
+    })  
+    
+ });
+ const showWinner = (winner) =>{
+        msg.innerText = 'Congratulations, you won the game ${winner}';
+ }
+ 
+ const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if(pos1val !="" && pos2val !="" && pos3val != ""){
+            if(pos1val===pos2val && pos2val ===pos3val){
+                console.log("winner",pos1val);
+                showWinner();
+            };
+        };
+    };
+    
+ };
